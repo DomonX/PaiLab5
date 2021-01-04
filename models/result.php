0487dd35ef48caacf3403cc239095cbd96572654
DomonX <norbiix1@gmail.com>
2021-01-04 18:59:18 +0100

    feat: models

---
+++ models/result.php
@@ -0,0 +1,13 @@
+<?php
+
+class Result {
+  public $id;
+  public $name;
+  public $groupNumber;
+  public $albumNumber;
+  public $grade;
+  public $comments;
+  public $password;
+}
+
+?>
---
+++ models/teacher.php
@@ -0,0 +1,9 @@
+<?php
+
+class Teacher {
+  public $id;
+  public $name;
+  public $password;
+}
+
+?>
