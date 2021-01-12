469d59317f1c9b0bee41f76780b61d45f31a7d04
DomonX <norbiix1@gmail.com>
2021-01-12 17:53:13 +0100

    fix, doc: Fix of pdf endpoint docs of endpoints

--- api/pdfEndpoint.php
+++ api/pdfEndpoint.php
@@ -2,12 +2,14 @@
     include_once($_SERVER['DOCUMENT_ROOT'].'/PaiLab5/pdfSaver/pdfSaver.php');
     include_once($_SERVER['DOCUMENT_ROOT'].'/PaiLab5/repository/resultDb/resultQuery.php');
 
-    if(!isset($_POST['testId'])) {
+    if(!isset($_POST['mode'])) {
       return;
     }
 
-    $resultQuery = new ResultQuery();
-    $tests = $resultQuery->getTestResults(7);
-    $pdf = new PdfSaver();
-    $pdf->save($tests);
+    if($POST['mode'] == 'create' && isset($_POST['testId'])) {
+      $resultQuery = new ResultQuery();
+      $tests = $resultQuery->getTestResults($_POST['testId']);
+      $pdf = new PdfSaver();
+      $pdf->save($tests);
+    }
 ?>
